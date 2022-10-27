7196841b06d5b357c86d3cfd334ea83539b3e11a
BirdFan <84983569+BirdFan@users.noreply.github.com>
2022-10-27 19:19:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import os 
 import threading
 import requests, random
-from dhooks import Webhook
-import ctypes
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import time
 
+time.sleep(1)
 
-
-def groupfinder():
-    id = random.randint(1000000, 1150000)
-    r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") 
-    if 'owned' not in r.text:
-        re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
-        if 'isLocked' not in re.text and 'owner' in re.text:
+def Finder():
+  id = random.randint(1, 115000000)
+  r = requests.get(f"https://roblox.com/groups/{id}")
+  if "owned" not in r.text:
+    re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
+    if 'isLocked' not in re.text and 'owner' in re.text:
             if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
-                hook.send(f'Hit: https://www.roblox.com/groups/group.aspx?gid={id}')
-                print(f"[+] Hit: {id}")
+              print(f"Group Found: {id}")
+              json = re.json
+              members = json['memberCount']
+              desc = json['description']
+              embed = DiscordEmbed(title='New Group Found! Group', color=242424)
+              embed.add_embed_field(name='ID', value=f'{id}')
+              embed.add_embed_field(name='Description', value=f'"{desc}"')
+              embed.add_embed_field(name='Members', value=f'{members}')
+              embed.add_embed_field(name='Link', value=f'https://www.roblox.com/groups/{id}')
+              webhook.add_embed(embed)
+              response = webhook.execute()
             else:
-                print(f"[-] No Entry Allowed: {id}")
-        else:
-            print(f"[-] Group Locked: {id}")
+              print(f"No entry: {id}")
     else:
-        print(f"[-] Group Already Owned: {id}")
-print("Finder Started")
-hook = "https://discord.com/api/webhooks/985655474156363796/K5WGxwRuteNHax94mQ6PXsN09u-zxWJBSA7SYyvTPRNhHqlcXAsZYOohdAsXktnNimDL"
-threads = int(100)
+      print(f"Locked: {id}")
+  else:
+    print(f"Owned: {id}")
+webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/985655474156363796/K5WGxwRuteNHax94mQ6PXsN09u-zxWJBSA7SYyvTPRNhHqlcXAsZYOohdAsXktnNimDL")
+threads = (int(input("How many threads: ")))
 while True:
     if threading.active_count() <= threads:
-        threading.Thread(target=groupfinder).start()
+        threading.Thread(target=Finder).start()
